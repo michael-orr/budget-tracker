768ec2c92161b3a3c91bd8e1a3730bf99ecdb433
Michael Orr <mike@glquote.com>
2021-11-18 09:13:38 -0800

    all file are here.

---
+++ public/service-worker.js
@@ -0,0 +1,59 @@
+let CACHE_NAME = 'static-cache-v1';
+let DATA_CACHE_NAME = 'data-cache-v1';
+
+let urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/icons/icon-512x512.png',
+    '/icons/icon-192x192.png',
+    '/db.js',
+    '/index.js',
+    '/manifest.json'
+  ];
+  
+
+  self.addEventListener("install", function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
+
+  self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+      return;
+    }
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
+        });
+      })
+    );
+  });
